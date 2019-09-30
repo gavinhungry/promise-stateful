6c295d9c8b48ad14273b16881427855d7088a1b0
Gavin Lloyd <gavinhungry@gmail.com>
2019-09-30 09:51:19 -0700

    Init commit

---
+++ promise-stateful.js
@@ -0,0 +1,10 @@
+/*
+ * promise-stateful - Native Promise wrapper with state getters
+ * https://github.com/gavinhungry/promise-stateful
+ */
+
+const PromiseStateful = () => {
+
+};
+
+module.exports = PromiseStateful;
