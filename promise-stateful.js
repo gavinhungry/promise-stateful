48c7e11a2e2f64ea6f877252fe2e6e235b917306
Gavin Lloyd <gavinhungry@gmail.com>
2019-09-30 10:04:09 -0700

    Add implementation

--- promise-stateful.js
+++ promise-stateful.js
@@ -3,8 +3,62 @@
  * https://github.com/gavinhungry/promise-stateful
  */
 
-const PromiseStateful = () => {
+/**
+ * Create a new promise the same way as you would with the Promise constructor:
+ *
+ *   let p = new PromiseStateful((res, rej) => {
+ *     // ...
+ *   });
+ *
+ *   p.isPending; // true
+ *   p.isFulfilled; // false
+ *   p.isRejected; // false
+ *
+ * Also works with Promise static methods:
+ *
+ *   let p = PromiseStateful.resolve('foo');
+ *
+ *   p.isPending; // false
+ *   p.isFulfilled; // true
+ */
+const PromiseStateful = Object.setPrototypeOf(function(fn) {
+  let states = {
+    isPending: true,
+    isFulfilled: false,
+    isRejected: false
+  };
 
+  let p = new Promise(fn).then(
+    val => {
+      states.isFulfilled = true;
+      return val;
+    },
+    err => {
+      states.isRejected = true;
+      throw err;
+    }
+  ).finally(() => {
+    states.isPending = false;
+  });
+
+  Object.keys(states).forEach(state => {
+    Object.defineProperty(p, state, {
+      enumerable: false,
+      get: () => states[state]
+    });
+  });
+
+  return p;
+}, Promise);
+
+/**
+ * Convenience method for creating a PromiseStateful from an existing Promise
+ *
+ * @param {Promise} p
+ * @return {PromiseStateful}
+ */
+PromiseStateful.from = p => {
+  return new PromiseStateful(p.then.bind(p));
 };
 
 module.exports = PromiseStateful;
